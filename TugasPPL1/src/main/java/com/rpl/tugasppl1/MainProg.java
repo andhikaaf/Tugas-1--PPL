71e6795fcd5b9196eee2585df3a5152f9ca64e59
esa89 <esa.fauzi@gmail.com>
2014-02-23 03:37:47 +0700

    Kelas MainProg Versi 1 (Awal)

---
+++ TugasPPL1/src/main/java/com/rpl/tugasppl1/MainProg.java
@@ -0,0 +1,30 @@
+package com.rpl.tugasppl1;
+
+import java.util.Vector;
+/**
+ * @author Esa Fauzi
+ * @since 23/02/2014
+ * @version 1.0
+ *
+ */
+public class MainProg {
+    public static void main( String[] args )
+    {
+//        ABB a = new ABB();
+//        P p = new P();
+//        Q q = new Q();
+        int N = 3;
+        Vector<Integer>[] v = new Vector[N];
+//        a.f();
+        for(int i=1;i<=N;i++){
+            v[i-1].add(i);
+            System.out.println("Nilai Vector-"+i+"= "+v[i-1]);
+        }
+        
+    }
+    
+//    @Override
+//    public int w() {
+//	return 2;
+//    }
+}
