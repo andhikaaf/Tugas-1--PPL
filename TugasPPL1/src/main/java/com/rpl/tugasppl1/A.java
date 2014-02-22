d85ed4c4d0596d28708764579d1ad00aba2fe905
junnotantra <junnotantra@gmail.com>
2014-02-23 03:05:31 +0700

    Add: java class (A) and interface (B, C)

---
+++ TugasPPL1/src/main/java/com/rpl/tugasppl1/A.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.rpl.tugasppl1;
+
+/**
+ *
+ * @author Junno
+ */
+public class A {
+    public A(){
+        System.out.println("ctor-A");
+    }
+    public void f(){
+        System.out.println("A");
+    }
+}
