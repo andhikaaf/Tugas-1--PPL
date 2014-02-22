cbed9dd1bc1ea09fb3a4c50fad1255634f9b2c21
Andhika-A-Firdaus <user@user-PC>
2014-02-23 04:14:26 +0700

    class turunan AB

---
+++ TugasPPL1/src/main/java/com/rpl/tugasppl1/AB.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.rpl.tugasppl1;
+
+/**
+ *
+ * @author Andhika
+ */
+public class AB extends A implements B, C {
+
+	public AB() {
+		System.out.println("ctor-AB");
+	}
+
+	@Override
+	public void f() {
+		System.out.println("AB.f()");
+	}
+	
+	@Override
+	public void v() {
+		System.out.println("AB.v()");
+	}
+
+	@Override
+	public void g() {
+		System.out.println("AB.g()");
+	}
+	
+}
