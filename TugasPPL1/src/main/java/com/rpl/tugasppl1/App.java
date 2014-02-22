ab18a7afa03c07b695157eda516dd3e22f80333b
junnotantra <junnotantra@gmail.com>
2014-02-23 01:52:07 +0700

    new

---
+++ TugasPPL1/src/main/java/com/rpl/tugasppl1/App.java
@@ -0,0 +1,13 @@
+package com.rpl.tugasppl1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
