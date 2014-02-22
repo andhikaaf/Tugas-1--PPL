b15a7dece45b07379616ddae30a603dea79a6e9d
esa89 <esa.fauzi@gmail.com>
2014-02-23 03:37:26 +0700

    Kelas AllConst Versi 1 (Awal)

---
+++ TugasPPL1/src/main/java/com/rpl/tugasppl1/AllConst.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.rpl.tugasppl1;
+
+/**
+ *
+ * @author Esa Fauzi
+ * @version 1.0
+ * @since 23/02/2014
+ * 
+ */
+public final class AllConst {
+    public static final double PI = Math.PI;
+    public static final double RHO = 9.0;
+    public static final double EPSILON = 0.005;
+    public static final int N = 8;
+}
