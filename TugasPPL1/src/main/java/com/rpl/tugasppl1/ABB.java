b672ee11b54657fb3990aa0e261d95f9b55ef2be
Andhika-A-Firdaus <user@user-PC>
2014-02-23 05:32:05 +0700

    class turunan B

---
+++ TugasPPL1/src/main/java/com/rpl/tugasppl1/ABB.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+
+* To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.rpl.tugasppl1;
+
+
+/**
+ *
+ * @author Andhika
+ */
+public class ABB extends AB implements B {
+
+	private int x;
+
+	public ABB() {
+		x = 0;
+		System.out.println("ctor-ABB");
+	}
+
+	public ABB(int x) {
+		this.x = x;
+		System.out.println("ctor-ABB (x = " + x + ")");
+	}
+
+	@Override
+	public void v() {
+		System.out.println("ABB.v()");
+	}
+
+	@Override
+	public void f() {
+		System.out.println("ABB.f()");
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	@Override
+	public String toString() {
+		return "" + x;
+	}
+}
